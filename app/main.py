8a1ef8de02cf1a85b8059c1205636e33957302a8
Ricardo Pachas Sifuentes <ricardops2211@gmail.com>
2025-09-15 11:15:36 -0500

    change

---
+++ app/main.py
@@ -0,0 +1,8 @@
+import os
+import datetime
+
+env = os.getenv("ENVIRONMENT", "DEV")
+print(f"🔥 Ejecutando aplicación en entorno: {env}")
+
+# Simular lógica de la app
+print("📦 App levantada correctamente!")
